a2579562afa8dd2260a217992c64b791773d4985
Nyirabazungu <vnyanki6@gmail.com>
2019-03-05 13:24:42 +0200

    add data that help to run

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
- export SECRET_KEY="0781200665"
-export MAIL_USERNAME=vnyanki6@gmail.com
+export SECRET_KEY="0781200665"
+export MAIL_USERNAME=vnyanki6
 export MAIL_PASSWORD=0781200665
 python3.6 manage.py server
