eb3ab93dcaf3f46444cf457eb85cf5acc8b38021
Nyirabazungu <vnyanki6@gmail.com>
2019-03-05 13:15:13 +0200

    define app

--- app/__init__.py
+++ app/__init__.py
@@ -7,28 +7,30 @@ from flask_uploads import UploadSet,configure_uploads,IMAGES
 from flask_mail import Mail
 from flask_simplemde import SimpleMDE
 
-bootstrap = Bootstrap()
-db = SQLAlchemy()
-simple = SimpleMDE()
+
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 photos = UploadSet('photos',IMAGES)
 mail = Mail()
-
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+simple = SimpleMDE()
 
 
 def create_app(config_name):
     app = Flask(__name__)
+    app.config.from_object(config_options[config_name]) 
+
 
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
     
     # Creating the app configurations
-    app.config.from_object(config_options[config_name]) 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
     # configure UploadSet
     configure_uploads(app,photos)
     # Initializing flask extensions
@@ -48,26 +50,4 @@ def create_app(config_name):
 
     return app
 
-# from flask import Flask
-# from .config import DevConfig
-# from flask_bootstrap import Bootstrap
-# from flask_sqlalchemy import SQLAlchemy
-
-# bootstrap = Bootstrap()
-# db = SQLAlchemy()
-
-# #  Initializing application
-# app = Flask(__name__,instance_relative_config = True)
-# def create_app(config_name):
-#     app = Flask(__name__)
-# # Setting up configuration
-# app.config.from_object(DevConfig)
-# app.config.from_pyfile("config.py")
-
-# # Initializing Flask Extensions
-# bootstrap = Bootstrap(app)
-# bootstrap.init_app(app)
-# db.init_app(app)
-# db.create_all()
 from .main import views
-# from .main import error
